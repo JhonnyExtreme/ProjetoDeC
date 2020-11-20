d1de6fecf3b000f7b113780e250cda84fdac1c81
Jhony Extreme <jhony.extreme2323@gmail.com>
2020-11-20 01:52:01 +0000

    arrumando função registro

--- main.c
+++ main.c
@@ -3,68 +3,91 @@
 #include <string.h>
 #include <math.h>
 
-int registro(){
+void registro(){
   double valor;
-  char descricao[150];
-  char data[11];
-  char categoria[20];
-  //Recebe um valor 
+  int dia,mes,ano;
+  char descricao[25];
+  char categoria[25];
   
-  puts("----------------------------------------------");
-  puts("Caso o Valor for Positivo Entra Como Receita");
-  puts("----------------------------------------------");
-  puts("Se o Valor for Negativo Entrara Como Gasto");
-  puts("----------------------------------------------");
-  scanf("%lf\n",&valor);
-  puts("Descrição");
-  puts("----------------------------------------------");
-  scanf("%s\n", descricao);
-  puts("Data(dd/mm/aaaa)");
-  puts("----------------------------------------------");
-  scanf("%s\n", data);
-FILE* fp = fopen ("Registros.txt", "r");  
-
-puts("----------------------------------------------");
-puts("");
-puts("");
-puts("");
-
-printf("As categorias EXISTENTES são:");
-
-char cat[25];
-char aux1[25];
-char aux2[25];
-char aux3[25];
-
-int i;
-int z = fscanf (fp, "%s %s %s %s", aux1,aux2,aux3,cat);
-for (i=0; z != EOF;i++) {
-    printf(" %s ",cat);
-    z = fscanf (fp, "%s %s %s %s", aux1,aux2,aux3,cat);
-}
-
-fclose(fp);
-
-FILE* f = fopen ("Registros.txt", "a+");
-  puts("");
-  puts("");
-  puts("----------------------------------------------");
-  puts("");
-  puts("Categoria");
-  puts("");
-  puts("----------------------------------------------");
+  FILE* f = fopen ("Registros.txt", "a+");
+  printf("Digite o valor da receita ou do gasto:\n");
+  scanf("%lf", &valor);
+  //colocar condição para não ter espaço 
+  printf("Descricao:\n");
+  scanf("%s",descricao);
+  printf("Ano:\n");
+  scanf("%d", &ano);
+  printf("Mês:\n");
+  scanf("%d", &mes);
+  if(mes>12 || mes<=0){
+    printf("Digite um mês válido\n");
+    scanf("%d",&mes);
+  }
+  printf("Dia:\n");
+  scanf("%d", &dia);
+  if(dia>= 32 || dia<=0){
+    printf("Digite um Dia válido\n");
+    scanf("%d", &dia);
+  } 
+  else if (mes == 1 && dia>31){
+    printf("Digite um Dia válido para o mês de janeiro.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 2 && dia>28){
+    printf("Digite um Dia válido para o mês de Fevereiro.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 3 && dia>31){
+    printf("Digite um Dia válido para o mês de março.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 4 && dia>30){
+    printf("Digite um Dia válido para o mês de abril.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 5 && dia>30){
+    printf("Digite um Dia válido para o mês de maio.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 6 && dia>30){
+    printf("Digite um Dia válido para o mês de junho.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 7 && dia>31){
+    printf("Digite um Dia válido para o mês de julho.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 8 && dia>31){
+    printf("Digite um Dia válido para o mês de Agosto.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 9 && dia>30){
+    printf("Digite um Dia válido para o mês de setembro.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 10 && dia>31){
+    printf("Digite um Dia válido para o mês de outubro.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 11 && dia>30){
+    printf("Digite um Dia válido para o mês de novembro.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 12 && dia>31){
+    printf("Digite um Dia válido para o mês de dezembro.\n");
+    scanf("%d", &dia);
+  }
+  else if (mes == 1 && dia>31){
+    printf("Digite um Dia válido para o mês de janeiro.\n");
+    scanf("%d", &dia);
+  }
+  printf("Digite o valor da receita ou do gasto:\n");
   scanf("%s", categoria);
-
-  fprintf(f,"%.2lf %s %s %s\n",valor,data,categoria,descricao);
-
-  fclose (f);
-  // por uma descrição
-  // por a data do registro
-  // indicar qual categoria.
-  // teste
-
-  return 0.0;
-
+  fprintf(f,"%.2lf %d %d %d %s %s\n",valor,dia,mes,ano,descricao,categoria);
+  fclose(f);
+  puts("");
+  puts("Registrado Com Sucesso!!");
+  puts("");
 }
 int menu(){
   int opcao=-1;
